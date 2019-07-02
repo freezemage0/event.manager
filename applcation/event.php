e796f0fe259b0546dfde268ca7722d4123f8c421
Veronika Merchalova <freezemage0@gmail.com>
2019-07-02 21:55:51 +0400

    initial commit

---
+++ applcation/event.php
@@ -0,0 +1,51 @@
+<?php
+class Event
+{
+	private $name;
+	private $parameters;
+	private $errors;
+	
+	public function __construct($name, array $parameters)
+	{
+		$this->name = $name;
+		$this->parameters = $parameters;
+	}
+	
+	public function getName()
+	{
+		return $this->name;
+	}
+	
+	public function getParameter($name)
+	{
+		if (isset($this->parameters[$name])) {
+			return $this->parameters[$name];
+		}
+		return null;
+	}
+	
+	public function getParameters()
+	{
+		return $this->parameters;
+	}
+	
+	public function setParameter($name, $value)
+	{
+		$this->parameters[$name] = $value;
+	}
+	
+	public function hasErrors()
+	{
+		return !empty($this->errors);
+	}
+	
+	public function getErrors()
+	{
+		return $this->errors;
+	}
+	
+	public function addError($message, $code)
+	{
+		$this->errors[$code] = $message;
+	}
+}
